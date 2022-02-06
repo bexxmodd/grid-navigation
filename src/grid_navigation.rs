e094fc73ddb322c21d8570813f38060736b8033e
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2022-02-05 21:56:28 -0500

    added path reconstruct and filtering of moves

--- src/grid_navigation.rs
+++ src/grid_navigation.rs
@@ -1,3 +1,4 @@
+use std::cmp::Reverse;
 use std::collections::{HashMap, VecDeque, BinaryHeap};
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug, Ord, PartialOrd)]
@@ -18,12 +19,29 @@ impl WeighterGrid {
         *self.weights.get(to).unwrap_or(&1.0)
     }
 
-    pub fn reconstruct_path(came_from: &HashMap<Location, Location>,
+    pub fn neighbors(&self, loc: &Location) -> Vec<Location> {
+        let x = loc.0;
+        let y = loc.1;
+        let neighbors = vec![(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)];
+        neighbors.iter()
+                 .filter(|(x, y)| self.in_bounds(&Location(*x, *y))
+                     && self.passable(&Location(*x, *y)))
+                 .map(|(x, y)| Location(*x, *y))
+                 .collect()
+    }
+
+    pub fn reconstruct_path(came_from: &HashMap<Location, Option<Location>>,
         start: &Location, goal: &Location) -> VecDeque<Location> {
         let mut current = goal;
         let mut path = VecDeque::new();
+
         while current != start {
-            current = &came_from[current];
+            let key = &came_from[current];
+            if let Some(k) = key {
+                current = k;
+            } else {
+                return path
+            }
             path.push_front(current.clone());
         }
         path
@@ -33,10 +51,42 @@ impl WeighterGrid {
         (from.0 as f32 - to.0 as f32).abs()
             + (from.1 as f32 - to.1 as f32).abs()
     }
+
+    fn in_bounds(&self, loc: &Location) -> bool {
+        loc.0 < self.rows && loc.1 < self.columns
+    }
+
+    fn passable(&self, loc: &Location) -> bool {
+        // TODO: add Grid member to the struct
+       true 
+    }
 }
 
 pub fn a_star_search(grid: &WeighterGrid, start: &Location, goal: &Location)
     -> VecDeque<Location> {
+        let mut frontier = BinaryHeap::new();
+        frontier.push(Reverse((0, start.clone())));
+        let mut came_from: HashMap<Location, Option<Location>> = HashMap::new();
+        let mut gscore_so_far: HashMap<Location, f32> = HashMap::new();
+        came_from.insert(start.clone(), None); 
+
+        while frontier.is_empty() {
+            let current = frontier.pop().unwrap().0;
+            if current.1 == *goal {
+                return WeighterGrid::reconstruct_path(&came_from, &start, &goal);
+            }
+
+            for neighbor in grid.neighbors(&current.1) {
+                let tent_gscore = gscore_so_far[&current.1]
+                    + grid.cost(&current.1, &neighbor);
+                if gscore_so_far.get(&neighbor).is_none()
+                    || tent_gscore < gscore_so_far[&neighbor] {
+                    gscore_so_far.insert(neighbor.clone(), tent_gscore);
+                    frontier.push(Reverse((tent_gscore as i32, neighbor.clone())));
+                }
+            }
+        }
+
         VecDeque::new()
 }
 
@@ -53,7 +103,7 @@ mod tests {
     #[test]
     fn reconstruct_path() {
         let mut path = HashMap::new();
-        path.insert(Location(0, 1), Location(0, 0));
+        path.insert(Location(0, 1), Some(Location(0, 0)));
         let mut expected = VecDeque::new();
         expected.push_front(Location(0, 0));
         assert_eq!(expected, WeighterGrid::reconstruct_path(
